83f8df17981b2c1c3dd360fcb8127bc1caaba755
Tim Jetten <tim.jetten.tj@gmail.com>
2014-09-12 21:03:50 +0200

    Initial Release

---
+++ BetterTools/src/main/java/net/greenwind/bettertools/BetterTools.java
@@ -0,0 +1,20 @@
+package net.greenwind.bettertools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Tim
+ */
+public class BetterTools extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+
+}
